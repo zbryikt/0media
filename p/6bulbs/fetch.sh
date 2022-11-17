c755db85af4fd7d598fb6f87f732b255f4ee59b7
zbryikt <tkirby@gmail.com>
2022-11-17 15:10:26 +0800

    regen for 3ff5129

--- p/6bulbs/fetch.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-wget -O data.json "https://docs.google.com/spreadsheets/d/1RiIPdwuzW7wjuSv4hrgAIxbjhntK6nDowmg99bvN8T0/gviz/tq"
--- t/qa/get-sample-json.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-wget -O sample.json "https://spreadsheets.google.com/feeds/list/10i17hvwS2YX5xMnn1nubEriVBYvB370ftdpmO2mjvOc/1/public/values?alt=json"
